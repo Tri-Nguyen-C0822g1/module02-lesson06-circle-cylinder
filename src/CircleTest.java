d12e4e1de85b5dd89a2e285478f7897a6c6f9513
Tri-Nguyen-C0822g1 <tringuyen10396@gmail.com>
2022-10-13 21:35:26 +0700

    add

---
+++ src/CircleTest.java
@@ -0,0 +1,8 @@
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle);
+        Circle circle1 = new Circle(3,"black");
+        System.out.println(circle1);
+    }
+}
---
+++ src/CylinderTest.java
@@ -0,0 +1,10 @@
+public class CylinderTest {
+    public static void main(String[] args) {
+    Cylinder cylinder = new Cylinder();
+        System.out.println(cylinder);
+    Cylinder cylinder1 = new Cylinder(3);
+        System.out.println(cylinder1);
+    Cylinder cylinder2 = new Cylinder(4, "red", 8);
+        System.out.println(cylinder2);
+    }
+}
